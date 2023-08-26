1659032bf06872b78c68bb9ab0787f146da522e5
CentarlSky <133049031+CentarlSky@users.noreply.github.com>
2023-08-27 01:35:10 +0900

    BashOperation.sh

--- BashOperation.sh
+++ BashOperation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# List of words to create operation names
+
 verbs=("Alpha" "Bravo" "Charlie" "Delta" "Echo" "Foxtrot" "Gamma" "Hawk" "Indigo" "Joker" "Kilo" "Lima" "Mamba" "Nova" "Omega" "Papa" "Quasar" "Romeo" "Sigma" "Tango" "Unity" "Victor" "Whiskey" "X-ray" "Yankee" "Zulu")
 adjectives=("Stealth" "Crimson" "Silent" "Thunder" "Iron" "Imperial" "Dead" "Fallen" "Sky" "Neptune" "Secondary Imperial" "Republic" "Fallen Democrat" "Eagle" "Golden" "Cobalt" "Azure" "Marine" "Emerald" "Ruby" "Scarlet" "Violet" "Bronze" "Platinum" "Sapphire" "Crystal" "Topaz" "Jade")
 nouns=("Phoenix" "Serpent" "Viper" "Falcon" "Shadow" "Zone" "Revelation" "Order" "Horizon" "Eclipse" "Comet" "Aurora" "Tempest" "Harmony" "Infinity" "Zenith" "Vortex" "Dragon" "Titan" "Monarch" "Nova" "Spectrum" "Apex" "Chronicle" "Quest" "Nebula" "Abyss" "Haven" "Oracle")
